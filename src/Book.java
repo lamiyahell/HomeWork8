3228cf589e00f186464652d23bff686d784e084f
lamiyahell <axira1108@gmail.com>
2023-04-28 22:56:44 +0300

    Доработан код. Объекты и классы

--- src/Book.java
+++ src/Book.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Book {
     private Author author;
     private String bookName;
@@ -24,5 +26,23 @@ public class Book {
 
     public void setPublishingYear(int publishingYear) {
         this.publishingYear = publishingYear;
+    }
+
+    @Override
+    public String toString() {
+        return author.toString() + " Книга: " + bookName + " Год публикации: " + publishingYear;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(author, book.author) && Objects.equals(bookName, book.bookName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(bookName);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,19 @@
 public class Main {
 
+    public static void printAuthor() {
+        System.out.println();
+    }
+
     public static void main(String[] args) {
         Author StephenKing = new Author("Стивен", "Кинг");
         Book theGreenMile = new Book(StephenKing,"Зеленая миля", 1996);
-        System.out.println("Автор: " + StephenKing.getFirstName() + " " + StephenKing.getLastName());
-        System.out.println("Книга: " + theGreenMile.getBookName());
-        System.out.println("Год публикации: " + theGreenMile.getPublishingYear());
+        System.out.println(theGreenMile);
 
         Author alanDeanFoster = new Author("Алан Дин", "Фостер");
         Book spellSinger = new Book(alanDeanFoster,"Чародей с гитарой", 1983);
-        System.out.println("Автор: " + alanDeanFoster.getFirstName() + " " + alanDeanFoster.getLastName());
-        System.out.println("Книга: " + spellSinger.getBookName());
-        System.out.println("Год публикации: " + spellSinger.getPublishingYear());
+        System.out.println(spellSinger);
+
         spellSinger.setPublishingYear(1985);
-        System.out.println("Год публикации: " + spellSinger.getPublishingYear());
+        System.out.println(spellSinger);
     }
 }
